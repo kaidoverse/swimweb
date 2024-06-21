d850ac369b0842b11967eb332ece351c8639d765
kaidoverse <kaidoverse@gmail.com>
2024-06-21 08:37:36 -0500

    added sticky effect to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import Swaglogo3 from '../../assets/Swaglogo3.png'
 
 const Navbar = () => {
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => [
+            window.scrollY > 50 ? setSticky(true) : setSticky(false)
+        ])
+    }, []);
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
     return (
-        <nav className='container'>
+        <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={Swaglogo3} alt="" className='Swaglogo3' />
-            <ul>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
                 <li>Home</li>
                 <li>Program</li>
                 <li>About us</li>
