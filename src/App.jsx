9ab424cc4258e4094ffd427cc29e741ca09766fa
kaidoverse <kaidoverse@gmail.com>
2024-06-21 08:08:35 -0500

    added title component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Program from './Components/Program/Program'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Program />
+      <div className='container'>
+        <Title subTitle='Our Programs' title='what we offer' />
+        <Program />
+      </div>
     </div>
   )
 }
