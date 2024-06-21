3300f57105fade5c1f36038c707548ee68a0d019
kaidoverse <kaidoverse@gmail.com>
2024-06-21 07:55:43 -0500

    added program component

--- src/Components/Program/Program.jsx
+++ src/Components/Program/Program.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Program.css'
+import teach1 from '../../assets/teach1.png'
+import image_1 from '../../assets/image-1.png'
+import image_2 from '../../assets/image-2.png'
+import image_3 from '../../assets/image-3.png'
 
-import teach1 from '../../assets/lesson.png'
-import program_icon_1 from '../../assets/program-icon-1.png'
-import program_icon_2 from '../../assets/program_icon_2.png'
-import program_icon_3 from '../../assets/program_icon_3.png'
+
+
 
 const Program = () => {
     return (
@@ -12,24 +14,25 @@ const Program = () => {
             <div className='programm'>
                 <img src={teach1} alt="" />
                 <div className='caption'>
-                    <img src={program_icon_1} alt="" />
+                    <img src={image_1} alt="" />
                     <p>Aqua Babies</p>
                 </div>
             </div>
             <div className='programm'>
                 <img src={teach1} alt="" />
                 <div className='caption'>
-                    <img src={program_icon_2} alt="" />
+                    <img src={image_2} alt="" />
                     <p>Young Adults</p>
                 </div>
             </div>
             <div className='programm'>
                 <img src={teach1} alt="" />
                 <div className='caption'>
-                    <img src={program_icon_3} alt="" />
+                    <img src={image_3} alt="" />
                     <p>Adults</p>
                 </div>
             </div>
+
         </div>
     )
 }
