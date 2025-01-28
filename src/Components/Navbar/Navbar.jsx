4aa0463cf48da57d24506e36b02b94af96b4b9b0
kaidoverse <kaidoverse@gmail.com>
2025-01-28 22:30:45 +0000

    updated mobile menu icons

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import swam_logo from '../../assets/swam-logo.png';
-import menu_icon from '../../assets/menu-icon.png';
 
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
@@ -42,25 +41,29 @@ const Navbar = () => {
     return (
         <nav className={`containerr ${sticky ? 'dark-nav' : ''}`}>
             <img src={swam_logo} alt="Logo" className="Swaglogo12" />
-            {mobileMenu}
+            {/* Overlay for mobile menu */}
+            {mobileMenu && (
+                <div className="mobile-menu-overlay" onClick={toggleMenu}></div>
+            )}
+            {/* Mobile menu */}
             <ul className={mobileMenu ? 'mobile-menu-open' : ''}>
                 <li>
-                    <Link to="/" onClick={() => setMobileMenu(false)}>
+                    <Link to="/" onClick={toggleMenu}>
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link to="/OurPackages" onClick={() => setMobileMenu(false)}>
+                    <Link to="/OurPackages" onClick={toggleMenu}>
                         Program
                     </Link>
                 </li>
                 <li>
-                    <Link to="/OurTeam" onClick={() => setMobileMenu(false)}>
+                    <Link to="/OurTeam" onClick={toggleMenu}>
                         About us
                     </Link>
                 </li>
                 <li>
-                    <Link to="/InstructPage" onClick={() => setMobileMenu(false)}>
+                    <Link to="/InstructPage" onClick={toggleMenu}>
                         Instructors
                     </Link>
                 </li>
@@ -68,12 +71,31 @@ const Navbar = () => {
                     <button className="bbtn">Contact us</button>
                 </li>
             </ul>
-            <img
-                src={menu_icon}
-                alt="Menu Icon"
-                className="menu-icon"
-                onClick={toggleMenu}
-            />
+            {mobileMenu ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="30"
+                    height="30"
+                    fill="white"
+                    className="menu-icon"
+                    onClick={toggleMenu}
+                >
+                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                </svg>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="30"
+                    height="30"
+                    fill="white"
+                    className="menu-icon"
+                    onClick={toggleMenu}
+                >
+                    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+                </svg>
+            )}
         </nav>
     );
 };
