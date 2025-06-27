c028e9acbdf57afb7a1743a3f5710283986158ad
kaidoverse <kaidoverse@gmail.com>
2025-06-27 12:09:27 +0100

    improved mobile nav and style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,110 +9,82 @@ const Navbar = () => {
     const location = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
-
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setSticky(true);
-            } else {
-                setSticky(false);
-            }
-        };
-
-        if (location.pathname === '/') {
-            window.addEventListener('scroll', handleScroll);
-        }
-
-        // Set sticky to true for all other pages
+        const handleScroll = () => setSticky(window.scrollY > 50);
         if (location.pathname === '/') {
             window.addEventListener('scroll', handleScroll);
         } else {
             setSticky(true);
         }
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        return () => window.removeEventListener('scroll', handleScroll);
     }, [location.pathname]);
 
-    // Close mobile menu on window resize (desktop view)
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth > 768) {
-                setMobileMenu(false);
-            }
+            if (window.innerWidth > 768) setMobileMenu(false);
         };
 
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    const toggleMenu = () => {
-        setMobileMenu(!mobileMenu);
+    useEffect(() => {
+        document.body.classList.toggle('no-scroll', mobileMenu);
+        return () => document.body.classList.remove('no-scroll');
+    }, [mobileMenu]);
+
+    const toggleMenu = (e) => {
+        e.preventDefault();
+        setMobileMenu(prev => !prev);
     };
 
-    const handleLinkClick = () => {
-        setMobileMenu(false);
-    };
+    const handleLinkClick = () => setMobileMenu(false);
 
     return (
         <nav className={`containerr ${sticky ? 'dark-nav' : ''}`}>
-            <img src={swam_logo} alt="Logo" className="Swaglogo12" />
-            {/* Overlay for mobile menu - only show when mobile menu is open */}
+            <img src={swam_logo} alt="SWAM Logo" className="Swaglogo12" />
+
             {mobileMenu && (
                 <div className="mobile-menu-overlay" onClick={toggleMenu}></div>
             )}
-            {/* Mobile menu */}
+
             <ul className={mobileMenu ? 'mobile-menu-open' : ''}>
+                {['/', '/OurPackages', '/OurTeam', '/InstructPage'].map((path, idx) => (
+                    <li key={idx}>
+                        <Link
+                            to={path}
+                            onClick={handleLinkClick}
+                            className={location.pathname === path ? 'active-link' : ''}
+                        >
+                            {path === '/' ? 'Home' :
+                                path === '/OurPackages' ? 'Program' :
+                                    path === '/OurTeam' ? 'About us' :
+                                        'Instructors'}
+                        </Link>
+                    </li>
+                ))}
                 <li>
-                    <Link to="/" onClick={handleLinkClick}>
-                        Home
+                    <Link to="/Contact" onClick={handleLinkClick}>
+                        <button className="bbtn">Contact us</button>
                     </Link>
-                </li>
-                <li>
-                    <Link to="/OurPackages" onClick={handleLinkClick}>
-                        Program
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/OurTeam" onClick={handleLinkClick}>
-                        About us
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/InstructPage" onClick={handleLinkClick}>
-                        Instructors
-                    </Link>
-                </li>
-                <li>
-                    <button className="bbtn">Contact us</button>
                 </li>
             </ul>
-            {mobileMenu ? (
+
+            <button className="menu-toggle-button" onClick={toggleMenu} aria-label="Toggle menu">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     width="30"
                     height="30"
                     fill="white"
-                    className="menu-icon"
-                    onClick={toggleMenu}
                 >
-                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                    {mobileMenu ? (
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                    ) : (
+                        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+                    )}
                 </svg>
-            ) : (
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="30"
-                    height="30"
-                    fill="white"
-                    className="menu-icon"
-                    onClick={toggleMenu}
-                >
-                    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-                </svg>
-            )}
+            </button>
         </nav>
     );
 };
