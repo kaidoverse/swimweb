15a085e462067d3738ef9ef66a4c8fdedc8aa3df
kaidoverse <kaidoverse@gmail.com>
2026-02-07 16:15:39 +0000

    up node engine to 24 and fix build

--- src/pages/ClientSays/ClientSays.jsx
+++ src/pages/ClientSays/ClientSays.jsx
@@ -1,12 +1,6 @@
-import React from 'react'
-import './ClientSays.css';
-import Hero from '../../pages/Home/sections/Hero/Hero';
-
 const ClientSays = () => {
     return (
-        <>
-            {/* <Hero /> */}
-        </>
+        <></>
     )
 }
 
